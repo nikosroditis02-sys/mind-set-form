f13bdb83283ea90942343357020bbc404ff57daa
nikosroditis02-sys <nikos.roditis02@gmail.com>
2026-04-18 14:36:02 +0300

    Delete app directory

--- app/layout.jsx
+++
@@ -1,9 +0,0 @@
-export const metadata = {
-title: "Mind Set Form",
-description: "Nutrition Office Website"
-};
-
-export default function RootLayout({ children }) {
-return ( <html lang="el"> <body>{children}</body> </html>
-);
-}
